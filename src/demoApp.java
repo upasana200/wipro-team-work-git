459f8fd8aa048da91ef70dba9e299de3aec0782d
upasana200 <upasanachatterjee2003@gmail.com>
2025-08-09 13:06:37 +0530

    Create demoApp.java

---
+++ src/demoApp.java
@@ -0,0 +1,10 @@
+class demoApp {
+  String appName = "Demo Application";
+
+  public static void main(String[] args) {
+
+    demoApp myApp = new demoApp();
+    System.out.println("My app's name is: " + demoApp.appName);
+
+  }
+}
